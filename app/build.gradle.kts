30e9c3b17bd017ecc1af533aab8130d2b9620c50
Roman Smirnov <s.roman3455@gmail.com>
2024-09-22 22:13:29 +0300

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
