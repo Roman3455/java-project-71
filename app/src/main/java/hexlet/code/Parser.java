4e3736f1eb31bc91527c8213de5e723897a16d79
Roman Smirnov <s.roman3455@gmail.com>
2024-10-15 19:42:27 +0300

    Remove useless format check in the Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -9,26 +9,18 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
 
 public class Parser {
-    private static final List<String> ACCEPTABLE_EXTENSIONS = List.of("json", "yaml", "yml");
 
     public static Map<String, Object> parse(String filepath) throws IOException {
-
         Path absolutePath = FileSystems.getDefault().getPath(filepath.trim()).toAbsolutePath().normalize();
         if (!Files.exists(absolutePath)) {
             throw new FileNotFoundException("File or directory not found");
         }
 
         var filename = absolutePath.getFileName().toString().split("\\.");
-
         var extension = filename[filename.length - 1];
-        if (!ACCEPTABLE_EXTENSIONS.contains(extension)) {
-            throw new IllegalArgumentException("File format is not supported");
-        }
-
         var content = Files.readString(absolutePath);
 
         ObjectMapper objectMapper;
